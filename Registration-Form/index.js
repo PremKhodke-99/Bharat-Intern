e4ad2428d97ef7263fb60f6bb40bafa82923a8a3
Prem <premkhodke.99@gmail.com>
2024-03-11 16:46:27 +0530

    index

--- Registration-Form/index.js
+++ Registration-Form/index.js
@@ -5,18 +5,18 @@ const mongoose = require('mongoose');
 
 const userRoutes = require("./routes/user.routes");
 
-const mongoDB_url = process.env.MongoDB_URL;
+// const mongoDB_url = process.env.MongoDB_URL;
 
-mongoose.connect(mongoDB_url)
-    .then(() => console.log("Connected to DB", mongoDB_url))
-    .catch((err) => console.log("Failed to connect", err));
+// mongoose.connect(mongoDB_url)
+//     .then(() => console.log("Connected to DB", mongoDB_url))
+//     .catch((err) => console.log("Failed to connect", err));
 
 const username = process.env.MONGODB_USERNAME;
 const password = process.env.MONGODB_PASSWORD;
 
-// mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.8d5atas.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
-//     .then(() => console.log("Connected to DB Online"))
-//     .catch((err) => console.error("Failed to connect", err));
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.8d5atas.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+    .then(() => console.log("Connected to DB Online"))
+    .catch((err) => console.error("Failed to connect", err));
 
 const app = express();
 const port = 8082;
