ad3979b53b02974286588a734f7a6907df254b66
Prem <premkhodke.99@gmail.com>
2024-03-19 04:58:09 +0530

    port

--- Blog-app/index.js
+++ Blog-app/index.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
 const path = require("path");
 const express = require("express");
-const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
-
+require("./db/conn");
 const Blog = require("./models/blog")
 
 const userRoute = require('./routes/user.routes');
@@ -11,14 +10,7 @@ const blogRoute = require('./routes/blog.routes');
 const { checkForAuthenticationCookie } = require("./middleware/authentication");
 
 const app = express();
-const PORT = 8000;
-
-const username = process.env.MONGODB_USERNAME;
-const password = process.env.MONGODB_PASSWORD;
-
-mongoose.connect(`mongodb+srv://${username}:${password}@blogcluster.azbz3rs.mongodb.net/?retryWrites=true&w=majority&appName=BlogCluster`)
-    .then(() => console.log("Connected to DB Online"))
-    .catch((err) => console.error("Failed to connect", err));
+const PORT = process.env.PORT || 8000;
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"))
