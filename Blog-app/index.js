a63e569a4a85dce0d24af4437263dd46209961de
Prem <premkhodke.99@gmail.com>
2024-03-18 15:23:18 +0530

    mongo

--- Blog-app/index.js
+++ Blog-app/index.js
@@ -16,13 +16,9 @@ const PORT = 8000;
 const username = process.env.MONGODB_USERNAME;
 const password = process.env.MONGODB_PASSWORD;
 
-mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.8d5atas.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+mongoose.connect(`mongodb+srv://${username}:${password}@blogcluster.azbz3rs.mongodb.net/?retryWrites=true&w=majority&appName=BlogCluster`)
     .then(() => console.log("Connected to DB Online"))
     .catch((err) => console.error("Failed to connect", err));
-
-// mongoose.connect("mongodb://localhost:27017/blogs")
-//     .then(() => console.log('MONGO DB Connected'))
-//     .catch((err) => console.error("Can't connect DB", err))
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"))
