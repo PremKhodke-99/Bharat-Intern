47ad1ef4ebee04861018bfd970bd4040897520dc
Prem <premkhodke.99@gmail.com>
2024-03-19 11:34:47 +0530

    new db

--- Blog-app/db/conn.js
+++ Blog-app/db/conn.js
@@ -3,6 +3,6 @@ const mongoose = require("mongoose");
 const username = process.env.MONGODB_USERNAME;
 const password = process.env.MONGODB_PASSWORD;
 
-mongoose.connect(`mongodb+srv://${username}:${password}@blogcluster.5hqka0f.mongodb.net/?retryWrites=true&w=majority&appName=BlogCluster`)
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.8d5atas.mongodb.net/blogapp`)
     .then(() => console.log("Connected to DB Online"))
     .catch((err) => console.error("Failed to connect", err));
